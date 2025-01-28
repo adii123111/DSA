598d60f819bcbaf445d301886ba5a84729f7b68a
Aditya Vishwakarma <128413151+adii123111@users.noreply.github.com>
2025-01-28 17:55:39 +0530

    Add files via upload

---
+++ ABCfloid.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "enter your no ";
+    cin >> n;
+    char num = 'A';
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i + 1; j++)
+        {
+            cout << num << " ";
+            num++;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
